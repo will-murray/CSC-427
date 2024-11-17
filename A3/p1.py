8da516a5d66f633ecfe5aee462738a9559aab138
Will M <williammurray29@gmail.com>
2024-11-17 04:19:22 +0000

    completed p2, p1 doesn't match expected output

--- A3/p1.py
+++ A3/p1.py
@@ -1,6 +1,9 @@
 import sys
 import re
+import graphviz
 
+
+assert(len(sys.argv) == 2)
 fasta_fname = sys.argv[1]
 
 def get_reads():
@@ -16,18 +19,19 @@ def max_prefix_suffix_match(str1, str2):
         k = len(str1) - l
         if(str1[:l] == str2[k:]):
             if len(str1[:l]) >= 10:
-                print(f"{str1[:l]}{str1[l:].lower()} : {str2[:k].lower()}{str2[k:]}")
-            return len(str1[:l])
+                return len(str1[:l])
         
     return 0
 
-def find_adjacencies(nodes):
+def build_dot_graph(nodes):
+    G = graphviz.Digraph()
+
     for i, node_i in enumerate(nodes):
         for j,node_j in enumerate(nodes):
             w = max_prefix_suffix_match(node_i,node_j)
-            if w > 10 and i != j:
-                print(f"{i} <-> {j} : {w}")
+            if w > 10 and i!=j:
+                G.edge(node_i,node_j,label=str(w))
 
-
+    G.render("overlap", format="png", cleanup=True)
     
-find_adjacencies(get_reads())
+build_dot_graph(get_reads())
